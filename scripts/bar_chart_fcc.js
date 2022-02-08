186b254b8660ff84228b45b714959acefdf28a81
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-02-08 17:49:31 +0530

    make extent working with fetched data

--- scripts/bar_chart_fcc.js
+++ scripts/bar_chart_fcc.js
@@ -1,8 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+    var winHeight = 0, 
+		winWidth = 0, 
+		timeout = false, 
+		delay = 350;
 
-var winHeight = 0, 
-				winWidth = 0, 
-				timeout = false, 
-				delay = 350;
+    var actualData;
+    var padding = 60;
+    var height = 30,
+        width = 40;
 
 function setCoreDimensions(){
 		winHeight = window.innerHeight;
@@ -19,8 +24,49 @@ window.addEventListener('resize', () => {
 		timeout = setTimeout(setCoreDimensions, delay)
 });
 
-/*Changes bar graph to be vertical and have year values on Y and amount on X useful for when height is more than width like in mobile phones*/
+/*Changes bar graph to be vertical and have year 
+ * values on Y and amount on X useful for when height 
+ * is more than width like in mobile phones*/
+
 function changeOrientation(){
 	return 0;
 }
+
+/* Fetching JSON data for the graph*/
+var dater;
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+    .then(function(response){
+        if(!response.ok){
+            console.log("There was an issue with the response: " + response.status + " " + response.statusText);
+        } 
+        return response.json();
+    })
+    .then(function(d){
+        dater = d;
+        actualData = dater.data;
+        initSvg();
+    })
+    .catch(function(err){
+        dater = "Unfortunately there was an error";
+        return dater;
+    });
+
+const initSvg = () => {
+/*initialising SVG*/
+const svg = d3.select("#visHolder")
+              .append("svg")
+              .attr("viewBox", '0 0 '+height+' '+width);
+
+/*setting scales and axes*/
+const xScale = d3.scaleTime()
+                 .domain([d3.extent(actualData, d => d[0])])
+                 .range(padding, width - padding);
+
+console.log("checking out d3 extent below");
+console.log(d3.extent(actualData, d => d[0]));
+    
+}
+
+});
+
 
