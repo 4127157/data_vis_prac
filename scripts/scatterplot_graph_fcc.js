543495e7cf27b7f028ece816527fdfd889a17249
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-02-18 07:16:46 +0530

    add both axes and colour circles

--- scripts/scatterplot_graph_fcc.js
+++ scripts/scatterplot_graph_fcc.js
@@ -13,7 +13,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     + response.status 
                     + " " 
                     + response.statusText);
-                visHolder.innerText = "Sorry there seems to be an error: "
+                visHolder.innerText = "There seems to be an error: "
                                     + response.status
                                     + " "
                                     + response.statusText;
@@ -33,20 +33,31 @@ window.addEventListener("DOMContentLoaded", () => {
             .attr("id", "tooltip")
             .style("opacity", 0);
 
+        
+
         var height = 450,
             width = 600, 
             padding = (height/3)*0.1;
-
+            
         const xScale = d3
             .scaleTime()
-            .domain(d3.extent(data, d => d.Year)
-                    .map(item => new Date(item)))
+            .domain([d3.min(data, d => d.Year)-1, d3.max(data, d => d.Year)+1 ])
             .range([padding*4, width-padding*2]);
+
+        const xScaleAxis = d3
+            .scaleTime()
+            .domain([new Date(d3.min(data, d => d.Year)-1,0,0), new Date(d3.max(data, d => d.Year)+1,0,0)])
+            .range([padding*4, width-padding*2])
+            .nice();
+
+        var maxYearScale = d3.max(data, d => {return xScale(d.Year)});
+            console.log(maxYearScale);
 
         const yScale = d3
             .scaleTime()
             .domain(d3.extent(data, d => new Date(1970,0,1,0, d.Time.split(":")[0], d.Time.split(":")[1])))
-            .range([padding, height-padding*2]);
+            .range([padding*1.5, height-padding*2])
+            .nice();
 
 
         const svg = d3
@@ -66,7 +77,21 @@ window.addEventListener("DOMContentLoaded", () => {
                     return yScale(new Date(1970,0,1,0, d.Time.split(":")[0], d.Time.split(":")[1]));
             })
             .style("opacity", 0.7)
+            .style("fill", (d) => {
+                            if(d.Doping == "")
+                                return '#4CAF50';
+                            else
+                                return '#3F51B5';
+            })
+            .style("stroke", "goldenrod")
+            .style("stroke-width", "1px")
             .on("mouseover", (e,d)=>{
+                let directionHorizontal = () => {
+                                if(xScale(d.Year) > maxYearScale*0.7)
+                                    return (x)-140+"px";
+                                else
+                                    return (x)+20+"px";
+                };
                 e.target.style.opacity = 0.9;
                 const[x,y] = d3.pointer(e, svg);
                 tooltip
@@ -80,7 +105,7 @@ window.addEventListener("DOMContentLoaded", () => {
                         Time : ${d.Time}<br/>
                         ${d.Doping}
                         `)
-                    .style("left", (x)+20+"px")
+                    .style("left",directionHorizontal())
                     .style("top", (y)+20+"px");
             })
             .on("mouseout", (e,d)=>{
@@ -91,6 +116,21 @@ window.addEventListener("DOMContentLoaded", () => {
                     .duration(400)
                     .style("opacity", 0);
             });
+
+        var xAxis = d3.axisBottom().scale(xScaleAxis);
+        svg
+            .append("g")
+            .call(xAxis)
+            .attr("id", "x-axis")
+            .attr("transform", `translate(0, ${height-padding*1.5})`);
+
+        var yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%M:%S'));
+
+        svg
+            .append("g")
+            .call(yAxis)
+            .attr("id", "y-axis")
+            .attr("transform", `translate(${padding*3.5}, 0)`);
             
     }
 })
