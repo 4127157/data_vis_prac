fa30de18ebc840ab0e4eaf48a1709785895675ad
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-02-25 20:41:48 +0530

    Add working tooltip and description

--- scripts/heat_map_fcc.js
+++ scripts/heat_map_fcc.js
@@ -8,7 +8,8 @@ window.addEventListener("DOMContentLoaded", () => {
                     initSvg(data);
                 });
             } else {
-                visHolder.innerText = `There seems to be an error: ${res.status} ${res.statusText}`;
+                visHolder.innerText 
+                    = `There seems to be an error: ${res.status} ${res.statusText}`;
             }
         })
         .catch ((err)=>{
@@ -16,12 +17,18 @@ window.addEventListener("DOMContentLoaded", () => {
         });
 
     const initSvg = (data) => {
-
+        
         let height = 900*0.6,
             width = 1600*0.6, 
             padding = (height/3)*0.1,
             rectHeight = padding,
             rectWidth = padding/2;
+
+        const tooltip = d3
+            .select("body")
+            .append("div")
+            .attr("id", "tooltip")
+            .style("opacity", 0);
 
         const svg = d3
             .select(`#visHolder`)
@@ -29,36 +36,24 @@ window.addEventListener("DOMContentLoaded", () => {
             .attr(`preserveAspectRatio`, `xMinYMin meet`)
             .attr(`viewBox`, `0 0 ${width} ${height}`);
 
-       /* console.log(data.monthlyVariance[0].year);*/
         const xScale = d3
             .scaleBand()
             .domain(Array.from(data.monthlyVariance, d=> d.year))
-                // // .map((item, i) => {
-                //     if(i==0){
-                        // return new Date(item, 0);
-                    // } else {
-                        // return new Date(item+1, 0);
-                    // }
-                // }))
             .range([padding*5, (width-padding)]);
-/*      console.log(xScale(new Date(1867,0)));*/
-        
 
         const yScale = d3
             .scaleBand()
             .domain(Array.from(data.monthlyVariance, d => d.month)
                 .map((d,i)=> {
-                        return d;
+                        return d-1;
                     
                 }))
             .range([padding,height-padding*4]);
 
         /*TODO: 
-         *      5) Make colour system that is not simply hardcoded array(borrow
-         *      from random quote project if needed)
-         *      6) Thoroughly investigate legend and add*/
+         *      6) Thoroughly investigate legend and add
+            *   7) Add tooltip*/
 
-        
         const getFillColor = (variance) => {
             let maxVar = d3.max(data.monthlyVariance, d => d.variance);
             let minVar = d3.min(data.monthlyVariance, d => d.variance);
@@ -72,7 +67,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     maxLight = 20;
                 
                 light = light + ((procVar/procMin)*(maxLight - light));
-                return `hsl(${hue}, ${sat}%, ${light}%)`;
+                return `hsl(${hue}, ${sat}%, ${Math.round(light)}%)`;
             }
 
             const getHotTemp = () => {
@@ -84,7 +79,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     maxLight = 25;
                 
                 light += ((procVar/procMin)*(maxLight - light));
-                return `hsl(${hue}, ${sat}%, ${light}%)`;
+                return `hsl(${hue}, ${sat}%, ${Math.round(light)}%)`;
             }
 
             if(variance < 0) {
@@ -104,10 +99,52 @@ window.addEventListener("DOMContentLoaded", () => {
             .append("rect")
             .attr(`class`, `cell`)
             .attr(`x`, d => xScale(d.year))
-            .attr(`y`, d => yScale(d.month))
+            .attr(`y`, d => yScale(d.month-1))
             .style(`fill`, d => getFillColor(d.variance))
-            .attr(`height`, yScale.bandwidth()+(yScale.bandwidth()*0.015) )
-            .attr(`width`, xScale.bandwidth()+(xScale.bandwidth()*0.18));
+            .attr(`height`, yScale.bandwidth()+(yScale.bandwidth()*0.015))
+            .attr(`width`, xScale.bandwidth()+(xScale.bandwidth()*0.18))
+            .attr('data-month', d=> d.month-1)
+            .attr('data-year', d => d.year)
+            .attr('data-temp', d => {
+                    return data.baseTemperature+(d.variance);
+            })
+            .on("mouseover", (e, d)=> {
+                const[x,y] = d3.pointer(e,svg);
+                
+                let date = new Date(0);
+                date.setUTCMonth(d.month);
+                let format = d3.timeFormat(`%B`);
+
+                tooltip
+                    .transition()
+                    .duration(200)
+                    .style("opacity", 0.9);
+
+                tooltip
+                    .html(`
+                        ${d.year} - ${format(date)}<br/>
+                        ${Math.round((data.baseTemperature+(d.variance)+Number.EPSILON)*100)/100}<br/>
+                        ${d.variance}`)
+                    .attr("data-year", `${d.year}`);
+                    
+                tooltip
+                    .style("left", `${(x)+5}px`)
+                    .style("top", `${(y)-75}px`);
+
+                e.target.setAttribute("width", xScale.bandwidth());
+                e.target.style.stroke = "black";
+                e.target.style.stroke_width = xScale.bandwidth()*0.04;
+            })
+            .on("mouseout", (e, d) => {
+                e.target.style.stroke = '';
+                e.target.style.stroke_width = '';
+                e.target.setAttribute("width", xScale.bandwidth()+(xScale.bandwidth()*0.18));
+
+                tooltip
+                    .transition()
+                    .duration(400)
+                    .style("opacity", 0);
+            });
 
 
         var yearTicks = []; 
@@ -125,15 +162,11 @@ window.addEventListener("DOMContentLoaded", () => {
                             }
                         }
                     });
-        console.log(yearTicks);
-
 
         var xAxis = d3
             .axisBottom()
             .scale(xScale)
             .tickValues(yearTicks);
-
-
 
         svg
             .append("g")
@@ -141,7 +174,6 @@ window.addEventListener("DOMContentLoaded", () => {
             .attr("id", "x-axis")
             .attr("transform", `translate(0, ${height-padding*3.5})`);
         
-
         var yAxis = d3
             .axisLeft()
             .scale(yScale)
@@ -152,8 +184,6 @@ window.addEventListener("DOMContentLoaded", () => {
                         let format = d3.timeFormat(`%B`);
                         return format(date);
                     });
-            // .tickArguments(["January", "February", "March", "April", "May", "June",
-            //         "July", "August", "September", "October", "November", "December"]);
 
         svg
             .append("g")
@@ -161,6 +191,13 @@ window.addEventListener("DOMContentLoaded", () => {
             .attr("id", "y-axis")
             .attr("transform", `translate(${padding*4.5}, 0)`);
 
-    }
 
+        document.getElementById('fromYr').innerText = data.monthlyVariance[0].year;  
+        document.getElementById('toYr').innerText = data.monthlyVariance[data.monthlyVariance.length-1].year;
+        document.getElementById('baseTemp').innerText = data.baseTemperature;
+                    
+
+
+
+    }
 });
